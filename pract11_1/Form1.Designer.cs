9bcc51c29235bfca2766f0aae0c055b21baa11e0
qrszy <157354093+qrszy@users.noreply.github.com>
2024-01-29 15:07:10 +0500

    Add files via upload

---
+++ pract11_1/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace pract11_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(9, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(9, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отчество";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(176, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Адрес";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 53);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(226, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(176, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Номер кредитной карточки";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(179, 118);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1874919424,
+            2328306,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(226, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label6.Location = new System.Drawing.Point(179, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Тип карты";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Дебет",
+            "Кредит"});
+            this.listBox1.Location = new System.Drawing.Point(179, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(142, 22);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label7.Location = new System.Drawing.Point(490, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 18);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Время звонков";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label8.Location = new System.Drawing.Point(482, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 18);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Междугородных";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label9.Location = new System.Drawing.Point(482, 156);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label9.Size = new System.Drawing.Size(83, 18);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Городских";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.numericUpDown2.Location = new System.Drawing.Point(485, 118);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(158, 21);
+            this.numericUpDown2.TabIndex = 16;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.numericUpDown3.Location = new System.Drawing.Point(485, 191);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(158, 21);
+            this.numericUpDown3.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(12, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 52);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "1-й абонент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button2.Location = new System.Drawing.Point(245, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 52);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "2-й абонент";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.Location = new System.Drawing.Point(485, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 52);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "3-й абонент";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(202, 57);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Сведения";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Abonent";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ pract11_1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static pract11_1.Form1;
+
+namespace pract11_1
+{
+    public partial class Form1 :Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+        public class Abonent
+        {
+            public string Name;
+            public string Fam;
+            public string otc;
+            public string Adres;
+            public double nomk;
+            public string tipk;
+            public double minme;
+            public double ming;
+
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            Abonent abonent1 = new Abonent();
+            abonent1.Name = textBox1.Text;
+            abonent1.Fam = textBox2.Text;
+            abonent1.otc = textBox3.Text;
+            abonent1.Adres = textBox4.Text;
+            abonent1.nomk = (double) numericUpDown1.Value;
+            abonent1.tipk = listBox1.Text;
+            abonent1.minme= (double) numericUpDown2.Value;
+            abonent1.ming = (double) numericUpDown3.Value;
+            if (abonent1.ming < 500)
+                MessageBox.Show("Абонент добавлен.\n Вводите новые данные");
+            else if (abonent1.ming >= 500)
+                MessageBox.Show(string.Format("Абоненты привысил кол-во минут \nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent1.Name, abonent1.Fam, abonent1.otc, abonent1.Adres, abonent1.nomk, abonent1.tipk));
+            else if (abonent1.minme > 0)
+                MessageBox.Show(string.Format("Абоненты пользовался междугородной связью.\nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent1.Name, abonent1.Fam, abonent1.otc, abonent1.Adres, abonent1.nomk, abonent1.tipk));
+
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            Abonent abonent2 = new Abonent();
+            abonent2.Name = textBox1.Text;
+            abonent2.Fam = textBox2.Text;
+            abonent2.otc = textBox3.Text;
+            abonent2.Adres = textBox4.Text;
+            abonent2.nomk = (double) numericUpDown1.Value;
+            abonent2.tipk = listBox1.Text;
+            abonent2.minme = (double) numericUpDown2.Value;
+            abonent2.ming = (double) numericUpDown3.Value;
+         if(abonent2.ming<500)MessageBox.Show("Абонент добавлен.\n Вводите новые данные");
+         else if (abonent2.ming >= 500)MessageBox.Show(string.Format("Абоненты привысил кол-во минут \nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent2.Name, abonent2.Fam, abonent2.otc, abonent2.Adres, abonent2.nomk, abonent2.tipk));
+         else if(abonent2.minme>0)MessageBox.Show(string.Format("Абоненты пользовался междугородной связью.\nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent2.Name, abonent2.Fam, abonent2.otc, abonent2.Adres, abonent2.nomk, abonent2.tipk));
+
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            Abonent abonent3 = new Abonent();
+            abonent3.Name = textBox1.Text;
+            abonent3.Fam = textBox2.Text;
+            abonent3.otc = textBox3.Text;
+            abonent3.Adres = textBox4.Text;
+            abonent3.nomk = (double) numericUpDown1.Value;
+            abonent3.tipk = listBox1.Text;
+            abonent3.minme = (double) numericUpDown2.Value;
+            abonent3.ming = (double) numericUpDown3.Value;
+            if (abonent3.ming < 500)
+                MessageBox.Show("Абонент добавлен.\n Вводите новые данные");
+            else if (abonent3.ming >= 500)
+                MessageBox.Show(string.Format("Абоненты привысил кол-во минут \nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent3.Name, abonent3.Fam, abonent3.otc, abonent3.Adres, abonent3.nomk, abonent3.tipk));
+            else if (abonent3.ming > 0)
+                MessageBox.Show(string.Format("Абоненты пользовался междугородной связью.\nФамилия: {0} \nИмя: {1}\nОтчество: {2} \nАдрес: {3} \nНомер карты: {4} \nТип карты: {5}", abonent3.Name, abonent3.Fam, abonent3.otc, abonent3.Adres, abonent3.nomk, abonent3.tipk));
+        }
+
+        private void button4_Click (object sender, EventArgs e)
+        {
+
+        }
+    }
+}
